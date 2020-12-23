2fdf08c64b3668d9b46a7a36bb9c2e283d123011
AmrKahla6 <amrkahla6@gmail.com>
2020-12-23 21:05:00 +0200

    Edit Home Page

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
-
 Auth::routes(['register' => false]);
+
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/{page}', 'AdminController@index');
